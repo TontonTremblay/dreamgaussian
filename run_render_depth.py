0d4a4a1c081fddca347ffcb909e8ba664b7468de
Fan-Yun Sun <sunfanyun@gmail.com>
2023-11-07 13:08:11 -0800

    adding depth output

---
+++ run_render_depth.py
@@ -0,0 +1,26 @@
+# add this k
+
+import subprocess 
+import glob 
+import os 
+from icecream import ic 
+print = ic 
+
+path = "zhenggang/data_DreamG/"
+
+for folder in glob.glob(path+"*/"):
+	out_name = folder.split('/')[-2]
+
+	files_obj = glob.glob(f"logs/{out_name}/*.obj")
+	# ic(files_obj)
+	# if f'logs/{out_name}/_mesh.obj' in files_obj:
+	# 	ic("continue")
+	# 	continue
+	# raise()
+	# to_call1 = f"python main.py --config configs/image.yaml input={folder}/input_img.png save_path={out_name}/"
+	# to_call2 = f"python main2.py --config configs/image.yaml input={folder}/input_img.png save_path={out_name}/"
+	to_call3 = f"python render_depth.py --config configs/image.yaml input={folder}/input_img.png save_path={out_name}/"
+	# subprocess.call(to_call1.split(" "))
+	# subprocess.call(to_call2.split(" "))
+	subprocess.call(to_call3.split(" "))
+	# raise()
